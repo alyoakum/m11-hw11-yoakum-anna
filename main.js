1b7c2ee5d06cc8ed5a14a792f3f4bef9272bf1a7
Anna <annayoakum@ufl.edu>
2019-11-10 17:24:24 -0600

    Define function for CSS color transition

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const myBoxes = document.getElementsByClassName('block');
 //The Functionality
 function loopThrough() {
 console.log(this.textContent.length)
+  const changeColor = () => this.classList.toggle('purple');
+  for (let i = 0; i < this.textContent.length; i++) {
+  };
 };
 
 //The Event Listener
