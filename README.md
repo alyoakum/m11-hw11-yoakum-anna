acf32241099b8c5919ee9ca9a079bdb9a68c6d7b
Anna <annayoakum@ufl.edu>
2019-11-10 17:32:21 -0600

    Revise style and update README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # m11-hw11-yoakum-anna
+
+I had no issues with this. No outside resources were used other than course lectures.
--- main.js
+++ main.js
@@ -3,12 +3,13 @@ const myBoxes = document.getElementsByClassName('block');
 
 //The Functionality
 function loopThrough() {
-console.log(this.textContent.length)
+
   const changeColor = () => this.classList.toggle('purple');
-  for (let i = 0; i < this.textContent.length; i++) {
-    for (let i = 0; i < this.textContent.length * 2; i++) {
+
+  for (let i = 0; i < this.textContent.length * 2; i++) {
     setTimeout(changeColor, i * 1000);
   };
+
 };
 
 //The Event Listener
